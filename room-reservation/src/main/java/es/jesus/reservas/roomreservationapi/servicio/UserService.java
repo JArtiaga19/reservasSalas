2c859124affa257e513e2ab3a275c9564e92c20f
Jesus <ja3@gmail.com>
2026-02-04 11:19:42 +0100

    implementar servicios de Room, User y Reservation para gestionar datos desde el controlador nuevo guardado

--- room-reservation/src/main/java/es/jesus/reservas/roomreservationapi/servicio/UserService.java
+++ room-reservation/src/main/java/es/jesus/reservas/roomreservationapi/servicio/UserService.java
@@ -20,34 +20,33 @@ public class UserService {
     public User save(User user) { // Guarda un usuario nuevo o actualiza uno existente en la base de datos
         return userRepository.save(user);
     }
-
 }
 
 /*
-    El Service es el “cerebro” del programa.
-    Aquí es donde el sistema piensa.
-    Aquí decides qué se puede hacer y qué no.
-    Aquí pones reglas, validaciones, comprobaciones.
-
-    Ejemplos reales:
-    “No dejar crear una sala con capacidad negativa.”
-    “No dejar reservar una sala que ya está ocupada.”
-    “Comprobar que el usuario existe antes de reservar.”
-    Todo eso no va en el controlador y no va en la base de datos.
-    Va en el Service, porque es la capa que toma decisiones.
-*/
+ * El Service es el “cerebro” del programa.
+ * Aquí es donde el sistema piensa.
+ * Aquí decides qué se puede hacer y qué no.
+ * Aquí pones reglas, validaciones, comprobaciones.
+ * 
+ * Ejemplos reales:
+ * “No dejar crear una sala con capacidad negativa.”
+ * “No dejar reservar una sala que ya está ocupada.”
+ * “Comprobar que el usuario existe antes de reservar.”
+ * Todo eso no va en el controlador y no va en la base de datos.
+ * Va en el Service, porque es la capa que toma decisiones.
+ */
 
 /*
-    El Repository es el “mensajero” que habla con la base de datos.
-    No piensa.
-    No decide nada.
-    Solo guarda, busca, borra o actualiza datos.
-
-    Ejemplos:
-    findAll() → trae todos los registros
-    save() → guarda un registro
-    deleteById() → borra un registro
-
-    El Repository es como un cajero automático:
-    solo te da o guarda dinero, pero no decide nada por ti.
-*/
+ * El Repository es el “mensajero” que habla con la base de datos.
+ * No piensa.
+ * No decide nada.
+ * Solo guarda, busca, borra o actualiza datos.
+ * 
+ * Ejemplos:
+ * findAll() → trae todos los registros
+ * save() → guarda un registro
+ * deleteById() → borra un registro
+ * 
+ * El Repository es como un cajero automático:
+ * solo te da o guarda dinero, pero no decide nada por ti.
+ */
